21a6d52b35891b72f49a1b9eaf5570f125765a33
aeon-tia <joeytheotter@gmail.com>
2024-10-15 18:41:18 -0700

    add autocomplete functions to cogs

--- cogs/adminonly.py
+++ cogs/adminonly.py
@@ -4,6 +4,24 @@ from typing import Self
 import discord
 from discord.ext import commands
 
+from src.ocular.operations import DataBase
+
+
+async def get_mount_names(ctx: discord.AutocompleteContext) -> list[str]:
+    """Fetch list of mount names for autocomplete."""
+    database = DataBase()
+    mounts = await database.list_item_names(
+        expansion=ctx.options["expansion"],
+    )
+    return mounts  # noqa: RET504
+
+
+async def get_expansion_names(ctx: discord.AutocompleteContext) -> list[str]:  # noqa: ARG001
+    """Fetch list of mount names for autocomplete."""
+    database = DataBase()
+    expansions = await database.list_expansions()
+    return expansions  # noqa: RET504
+
 
 class AdminOnly(commands.Cog):
     """Class to hold admin only commands."""
