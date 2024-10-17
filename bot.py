927323edd1a619157748736568d6e947d3bb265f
aeon-tia <joeytheotter@gmail.com>
2024-10-16 19:06:05 -0700

    simplify formatting

--- bot.py
+++ bot.py
@@ -12,7 +12,7 @@ from src.ocular.operations import DataBase
 logger = logging.getLogger("discord")
 logger.setLevel(logging.INFO)
 formatter = logging.Formatter(
-    "{asctime} | {levelname:8s} | {funcName}:{lineno} {message}",
+    "{asctime} | {levelname} | {funcName}:{lineno} {message}",
     style="{",
 )
 handler = RotatingFileHandler(
